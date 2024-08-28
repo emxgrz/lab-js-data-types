e8f9c6fdac972ea731443fc74c9b51291f47a70c
Emma <emmamartinezgarcia2806@gmail.com>
2024-08-28 16:30:25 +0200

    terminado

--- index.js
+++ index.js
@@ -9,11 +9,10 @@ const s5 = "and";
 
 // Concatenate the string variables into one new string
 
-
+let tongueTwister = (`${s1} ${s2} ${s3} ${s4} ${s5} ${s3} ${s2} ${s1} ${s4}`)
 // Print out the concatenated string
 
-
-
+console.log(tongueTwister)
 
 /*******************************************
     Iteration 1.2 | Camel Tail
@@ -22,10 +21,13 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
+lastLetter1 = part1.slice(0,-1) + part1.slice(-1).toUpperCase()
+lastLetter2 = part2.slice(0,-1) + part2.slice(-1).toUpperCase()
 
+resultado = lastLetter1+lastLetter2 
 
 // Print the cameLtaiL-formatted string
-
+console.log(resultado)
 
 
 
@@ -36,8 +38,12 @@ const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
 
+let tip = billTotal *0.15
+
 
 // Print out the tipAmount
+
+console.log(tip)
 
 
 
@@ -47,11 +53,12 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
+let random = Math.random()*10
 
 
 // Print the generated random number
 
-
+console.log(random)
 
 /*******************************************
     Iteration 3.1 | Booleans
@@ -61,16 +68,23 @@ const a = true;
 const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
-const expression1 = a && b;
+const expression1 = a && b; 
+//false
 
 const expression2 = a || b;
+//true
 
 const expression3 = !a && b;
+//false
 
 const expression4 = !(a && b);
+//true
 
 const expression5 = !a || !b;
+//true
 
 const expression6 = !(a || b);
+//false
 
 const expression7 = a && a;
+//true
